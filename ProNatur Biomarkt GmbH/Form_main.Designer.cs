3d064e5449ff34095a3308f42f569693055ef203
Peter Hassen <pluto64@e-mail.de>
2022-02-16 17:14:44 +0100

    Remove and Add
    Bilder neu und alt

--- ProNatur Biomarkt GmbH/Form_main.Designer.cs
+++ ProNatur Biomarkt GmbH/Form_main.Designer.cs
@@ -29,22 +29,42 @@ namespace ProNatur_Biomarkt_GmbH
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_main));
+            this.button_products = new System.Windows.Forms.Button();
+            this.button_bill = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // panel1
+            // button_products
             // 
-            this.panel1.Location = new System.Drawing.Point(146, 49);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(444, 320);
-            this.panel1.TabIndex = 0;
+            this.button_products.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_products.BackgroundImage")));
+            this.button_products.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_products.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_products.Location = new System.Drawing.Point(21, 22);
+            this.button_products.Name = "button_products";
+            this.button_products.Size = new System.Drawing.Size(275, 125);
+            this.button_products.TabIndex = 0;
+            this.button_products.Text = "Produkte verwalten";
+            this.button_products.UseVisualStyleBackColor = true;
+            // 
+            // button_bill
+            // 
+            this.button_bill.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_bill.BackgroundImage")));
+            this.button_bill.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_bill.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_bill.Location = new System.Drawing.Point(314, 22);
+            this.button_bill.Name = "button_bill";
+            this.button_bill.Size = new System.Drawing.Size(275, 125);
+            this.button_bill.TabIndex = 1;
+            this.button_bill.Text = "Rechnung stellen";
+            this.button_bill.UseVisualStyleBackColor = true;
             // 
             // Form_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(612, 170);
+            this.Controls.Add(this.button_bill);
+            this.Controls.Add(this.button_products);
             this.Name = "Form_main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hauptmenue";
@@ -54,6 +74,7 @@ namespace ProNatur_Biomarkt_GmbH
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_products;
+        private System.Windows.Forms.Button button_bill;
     }
 }
